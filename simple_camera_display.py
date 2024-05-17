e1e9ae9c74c4bb3ab6262ca00b8ac273a68c1052
ASRobotics <102324508+alesaldii@users.noreply.github.com>
2024-05-17 13:42:23 -0600

    Update simple_camera_display.py

--- simple_camera_display.py
+++ simple_camera_display.py
@@ -1,33 +1,61 @@
+"""
+Title:        Simple Camera Line Folling Code
+Objective:    Use camera to indicate via print() funct. to turn right, left or go straight.
+Methodology:  Huge Line Transform Method is used taking theta as reference to go straigh.
+Author:       asaldivar
+"""
+
 import cv2
+import numpy as np
+import math
 
 def main():
-    # Open a connection to the first camera (index 0)
-    cap = cv2.VideoCapture(0)
 
-    # Check if the camera opened successfully
+    cap = cv2.VideoCapture(0)  # 0 for the default camera
+    
     if not cap.isOpened():
-        print("Error: Could not open camera.")
+        print("Error: Unable to open camera.")
         return
 
-    while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Error: Unable to capture frame.")
+                break
+            
+            """Edge Detection"""
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #grayscale
+            blur = cv2.GaussianBlur(gray,(5,5),0) #gaussian blur
+            ret,thresh = cv2.threshold(blur,60,255,cv2.THRESH_BINARY_INV) #thresholding
+            edges = cv2.Canny(thresh,100,200) #finding edges
+            
+            """Probabilistic Huge Line Transform"""
+            #https://docs.opencv.org/3.4/d9/db0/tutorial_hough_lines.html
+            theta = 0
+            lines = cv2.HoughLinesP(edges.copy(), 1, np.pi/180, 10, None, 50, 10)
+            if lines is not None:
+                for line in lines:
+                    x1, y1, x2, y2 = line[0]
+                    cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 3) #drawing lines on frame
+                    theta=theta+math.atan2((y2-y1),(x2-x1)) #theta calculation
+                #print(theta)
+                threshold = 7
+                if theta > threshold:
+                    print("Turn Left")
+                elif theta < -threshold:
+                    print("Turn Right")
+                else:
+                    print("Go Straight")
 
-        # If frame is read correctly, ret is True
-        if not ret:
-            print("Error: Can't receive frame (stream end?). Exiting ...")
-            break
+            cv2.imshow('Camera', frame)
 
-        # Display the resulting frame
-        cv2.imshow('Camera', frame)
-
-        # Exit the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release the camera
-    cap.release()
-    cv2.destroyAllWindows()
-
+            if cv2.waitKey(1) & 0xFF == ord('q'): #press q to exit
+                break
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+        
+            
 if __name__ == "__main__":
     main()
