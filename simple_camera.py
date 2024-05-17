3d82a4ffca1263ac04f74fd84c67c40b6e2194c9
ASRobotics <102324508+alesaldii@users.noreply.github.com>
2024-05-17 12:29:38 -0600

    Update simple_camera.py

--- simple_camera.py
+++ simple_camera.py
@@ -1,18 +1,36 @@
 """
-Title: Simple Open Camera and Display the Image
-Author: alesaldii
+Title:        Simple Camera Code
+Author:       asaldivar
 """
+
 import cv2
 
-cap = cv2.VideoCapture(0)
-if cap.isOpened():
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("Error: unable to open camera")
-            break
-        cv2.imshow('Camera',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-else:
-    print("Error: unable to open camera")
+
+def main():
+
+    cap = cv2.VideoCapture(0)  # 0 for the default camera
+    
+    if not cap.isOpened():
+        print("Error: Unable to open camera.")
+        return
+
+    try:
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Error: Unable to capture frame.")
+                break
+            
+           
+
+            cv2.imshow('Camera', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'): #press q to exit
+                break
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+        
+            
+if __name__ == "__main__":
+    main()
